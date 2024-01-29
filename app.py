34555124c20daf443a43d0d3bc7bcc706f19ccc6
Huclark <vhuclark@gmail.com>
2024-01-29 22:56:06 +0000

    Update: Todo class and create_app()

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # __name__ just references the file
 app = Flask(__name__)
+# tell the app where our databse is located
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# initialise our database
+db = SQLAlchemy(app)
 
+# define a function to create the app
+def create_app():
+    with app.app_context():
+        db.create_all()
+    return app
+
+# create a class
+class Todo(db.Model):
+    """For the todo app 
+    """
+    # create columns
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __str__(self):
+        """string representation of class"""
+        return f"<Task {self.id}>"
+    
 # create an index route so that when we browse to the URL we dont get error 404
 @app.route('/')
 def index():
