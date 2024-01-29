d58b21fb765525e2023bb6b33ffe536bc6c47012
Huclark <vhuclark@gmail.com>
2024-01-29 21:00:00 +0000

    Add: the app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+# __name__ just references the file
+app = Flask(__name__)
+
+# create an index route so that when we browse to the URL we dont get error 404
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # debug=True so when there is an error we can get a message to debug
+    app.run(debug=True)
