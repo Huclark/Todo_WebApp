8cceeb82f59d87483f4dda47ec72a1c245ac0eea
Huclark <vhuclark@gmail.com>
2024-01-31 17:59:30 +0000

    Update: imports and config

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
+
 # __name__ just references the file
-app = Flask(__name__)
-# tell the app where our databse is located
+app = Flask(__name__)  # create a Flask application
+# Secret key for form security
+app.config['SECRET_KEY'] = 'b818346b8a50ca0f5e38dd6670a0b475'
+# tell the app where our database is located (SQLite database URI)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 # initialise our database
 db = SQLAlchemy(app)
+# initialise a Flask-login extension
+login_manager = LoginManager(app)
+# Specify the login view for Flask-Login
+login_manager.login_view = 'login'
 
-# define a function to create the app
-def create_app():
-    with app.app_context():
-        db.create_all()
-    return app
+# # define a function to create the app
+# def create_app():
+#     with app.app_context():
+#         db.create_all()
+#     return app
 
 # create a class
 class Todo(db.Model):
@@ -21,13 +33,15 @@ class Todo(db.Model):
     """
     # create columns
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __str__(self):
         """string representation of class"""
-        return f"<Task {self.id}>"    
+        return f"<Task {self.id}>"
+    
     
 # create an index route so that when we browse to the URL we dont get error 404
 @app.route("/", methods=["POST", "GET"])
@@ -92,5 +106,13 @@ def update(id):
         return render_template("update.html", task=update_task)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        # Create a default user if needed
+        if not User.query.filter_by(username='default').first():
+            default_user = User(username='default')
+            default_user.set_password('taskmanager235802.')
+            db.session.add(default_user)
+            db.session.commit()
     # debug=True so when there is an error we can get a message to debug
     app.run(debug=True)
